1fdf3bf02d6214de1ab0705d85faf4a4768c2f0a
rscarlett1 <raewynne.scarlett@gmail.com>
2016-11-13 21:04:38 +1300

    start footer

--- footer.php
+++ footer.php
@@ -8,30 +8,52 @@
  
 <footer>
     <div class="container">
-        <div class="col-sm-3">
-            <a href="/">
-        <?php if ( ot_get_option( 'Navbar_logo_horse' ) ) { ?>
+        <div class="row"> 
+            <div id="footer-free" class="col-sm-12">
+            <a href="/"></a>
+            <?php if ( ot_get_option( 'Navbar_logo_horse' ) ) { ?>
             <img src="<?php echo esc_url( ot_get_option( 'Navbar_logo_horse' ) ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>">
-        <?php } ?>
-            <img src="<?php bloginfo('template_directory'); ?>/assets/img/logo.png" alt="Bootstrap to WordPress"></a>
-         </div><!-- end col 3 -->
+            <?php } ?>
 
-        <div class="col-sm-6">
-        <?php wp_nav_menu( array( 'menu' => 'ul',
-            'theme_location'    => 'secondary',
-            'menu_class'         => 'list-unstyled list-inline',
-            ));
-            ?>
-        </div><!-- end col sm 6 -->
+            <p class="text-center panel">Free Shipping On Orders Over &#36;100 in New Zealand</p>
+            </div><!-- end col 12 -->
+        </div>
+
+       <div class="row">
+            <div id="links" class="col-sm-3">
+                <?php wp_nav_menu( array( 'menu' => 'ul',
+                    'theme_location'    => 'secondary',
+                    'menu_class'         => 'list-unstyled list-inline',
+                    ));
+                    ?>
+                <ul>
+                    <li><a href="">Blog</a></li>
+                    <li><a href="">Contact</a></li>
+                    <li><a href="">FAQs</a></li>
+                    <li><a href="">Returns and Shipping</a></li>
+                    <li><a href="">Testimonials</a></li>
+                    <li><a href="">Customer Support</a></li>
+                </ul>
+            </div><!-- end col sm 3 -->
+        
  
- 
-        <div class="col-sm-3">
-            <div class="site-info">
-                <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'wirosableng' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'wirosableng' ), 'WordPress' ); ?></a>
-                <span class="sep"> | </span>
-                <?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'wirosableng' ), 'wirosableng', '<a href="http://seegatesite.com" rel="designer">Sigit Prasetya Nugroho</a>' ); ?>
-            </div><!-- .site-info -->
-        </div><!-- end of col -->
+   
+            <div class="col-sm-6">
+            
+                <p>Become a Love Of Horses VIP</p>
+
+                <form>
+                    <div class="form-group">
+                    <label for="exampleInputEmail1">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+                    <button type="submit" class="btn">Submit</button>
+                    </div>
+                </form>
+            </div><!-- col  -->
+    
+
+
+        </div><!-- row -->
     </div><!-- container -->
 </footer>
  
